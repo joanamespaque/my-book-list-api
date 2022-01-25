0aad5fc506e32c45f80af5d376bd38b74de50180
joanamespaque <joanamespaque@gmail.com>
2022-01-25 07:48:22 -0300

    change ssl config again

--- config/database.js
+++ config/database.js
@@ -80,7 +80,7 @@ module.exports = {
       user: Env.get("DB_USER", "postgres"),
       password: Env.get("DB_PASSWORD", ""),
       database: Env.get("DB_DATABASE", "adonis"),
-      ssl: Env.get("SSL", true),
+      ssl: { rejectUnauthorized: false },
     },
     debug: Env.get("DB_DEBUG", false),
   },
